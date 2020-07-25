9f3652087dce8b24ab0cc3e9e97bedb309fc7ec6
Feng Jing <yukuns@163.com>
2020-07-25 09:55:12 +0800

    change few lines

--- PyTorch-and-CV/C3/code.py
+++ PyTorch-and-CV/C3/code.py
@@ -6,18 +6,21 @@ print("Python version:{}".format(platform.python_version()))
 
 import time
 import torch
-## use CPU
-# for i in range(1,10):
-#     start = time.time()
-#     a = torch.FloatTensor(i*100,1000,1000)
-#     a = torch.matmul(a,a)
-#     end = time.time()-start
-#     print(end)
+# use CPU
+for i in range(1,20):
+    start = time.time()
+    a = torch.FloatTensor(i*100,1000,1000)
+    a = torch.matmul(a,a)
+    end = time.time()-start
+    print(end)
 print('---')
 
 ## use GPU
 print(torch.cuda.is_available())
-for i in range(1,10):
+print(torch.cuda.device_count())
+print(torch.cuda.get_device_name())
+print(torch.cuda.current_device())
+for i in range(1,20):
     start = time.time()
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     a = torch.FloatTensor(i*100,1000,1000)
