b27793c0c4e406ec35e5e9fe1e1598a6e2b1caae
Feng Jing <yukuns@163.com>
2020-08-03 14:56:20 +0800

    add new files

---
+++ ssd.pytorch-master/test1.py
@@ -0,0 +1,16 @@
+from data import *
+from utils.augmentations import SSDAugmentation
+from layers.modules import MultiBoxLoss
+from ssd import build_ssd
+import os
+import sys
+import time
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+import torch.nn.init as init
+import torch.utils.data as data
+import numpy as np
+import argparse
